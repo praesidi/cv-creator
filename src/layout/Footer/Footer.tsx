ab1d8b321ab7e15e425a28fcf02021f53ad33934
Elmir Khalilov <edik.halilov1999@gmail.com>
2023-05-23 17:34:40 +0400

    Organize page layout and file structure

---
+++ src/layout/Footer/Footer.tsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import "./Footer.css";
+
+export default function Footer() {
+  return (
+    <div className="footer">
+      <p>
+        © Made by
+        <a href="https://github.com/Praesidi" target="_blank">
+          Praesidi
+        </a>
+        for
+        <a href="https://www.theodinproject.com/" target="_blank">
+          The Odin Project
+        </a>
+      </p>
+    </div>
+  );
+}
---
+++ src/layout/Header/Header.tsx
@@ -0,0 +1,10 @@
+import { useState } from "react";
+import "./Header.css";
+
+export default function Header() {
+  return (
+    <div className="header">
+      <h1>CV Creator</h1>
+    </div>
+  );
+}
