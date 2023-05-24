bddad16dc2098cfcaa902ac182a3b4ebf49682b5
Elmir Khalilov <edik.halilov1999@gmail.com>
2023-05-24 22:15:04 +0400

    Build main component to handle all components

--- src/layout/Main/Main.tsx
+++ src/layout/Main/Main.tsx
@@ -1,10 +1,21 @@
 import { useState } from "react";
+import General from "../../components/General/General";
+import PreviousWork from "../../components/Experience/Experience";
+import Education from "../../components/Education/Education";
+import Skills from "../../components/Skills/Skills";
+import Overview from "../../components/Overview/Overview";
 import "./Main.css";
+
+//TODO: if edit mode is ON show inputs else show ready CV
 
 export default function Main() {
   return (
     <div className="main">
-      <div></div>
+      <General></General>
+      <PreviousWork></PreviousWork>
+      <Education></Education>
+      <Skills></Skills>
+      <Overview></Overview>
     </div>
   );
 }
