177cc95727bf65b978d84ebb399887798be33787
Elmir Khalilov <edik.halilov1999@gmail.com>
2023-05-26 22:01:19 +0400

    Add a component for handling inputs for Education info

--- src/components/Education/Education.tsx
+++ src/components/Education/Education.tsx
@@ -1,5 +1,71 @@
 import "./Education.css";
 
 export default function Education() {
-  return <div></div>;
+  return (
+    <div className="education-container block">
+      <h2>Education</h2>
+      <input
+        type="text"
+        id="university"
+        name="university"
+        placeholder="University Name"
+        maxLength={30}
+        // value={work.title}
+        // onChange={onChange}
+      />
+
+      <input
+        type="text"
+        id="city"
+        name="city"
+        placeholder="City"
+        maxLength={20}
+        // value={work.phone}
+        // onChange={onChange}
+      />
+
+      <input
+        type="text"
+        id="degree"
+        name="degree"
+        placeholder="Degree"
+        maxLength={20}
+        // value={work.phone}
+        // onChange={onChange}
+      />
+
+      <input
+        type="text"
+        id="subject"
+        name="subject"
+        placeholder="Subject"
+        maxLength={20}
+        // value={work.phone}
+        // onChange={onChange}
+      />
+
+      <input
+        type="text"
+        id="from"
+        name="from"
+        placeholder="From"
+        maxLength={20}
+        // value={work.phone}
+        // onChange={onChange}
+      />
+
+      <input
+        type="text"
+        id="to"
+        name="to"
+        placeholder="To"
+        maxLength={20}
+        // value={work.phone}
+        // onChange={onChange}
+      />
+
+      <button>Add</button>
+      <button>Delete</button>
+    </div>
+  );
 }
